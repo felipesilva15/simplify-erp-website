42b47dd2467d6e072a0d915ff3718fd30379f307
Felipe Silva <felipe.allware@gmail.com>
2026-02-02 22:10:20 -0300

    feat: Instalando e configurando as bibliotecas do PrimeNG e Primeflex

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,27 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    providePrimeNG({
+      ripple: true,
+      inputStyle: 'outlined',
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: 'p',
+          darkModeSelector: '.dark-mode',
+          cssLayer: false
+        }
+      }
+    })
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, ButtonModule],
+  template: `
+    <p-button label="test" severity="secondary"></p-button>
+    <router-outlet />
+  `
 })
 export class App {
   protected readonly title = signal('simplify-erp');
