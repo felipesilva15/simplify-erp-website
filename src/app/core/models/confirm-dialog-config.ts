d5d4eba258a66596ef7e7b68205daf538826d7ce
felipe.silva <felipe.silva@pwi.com.br>
2026-03-02 20:01:52 -0300

    feat: Criando service para ConfirmDialog

--- src/app/core/models/confirm-dialog-config.ts
+++ src/app/core/models/confirm-dialog-config.ts
@@ -1,3 +1,9 @@
+import { ButtonProps } from "primeng/button";
+
 export interface ConfirmDialogConfig {
+    header?: string;
     message: string;
+    icon?: string;
+    rejectButtonProps?: { label?: string, severity?: string, outlined?: boolean },
+    acceptButtonProps?: { label?: string, severity?: string, outlined?: boolean },
 }
--- src/app/shared/services/confirm-dialog-service.ts
+++ src/app/shared/services/confirm-dialog-service.ts
@@ -1,14 +1,36 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { ConfirmDialogConfig } from '../../core/models/confirm-dialog-config';
+import { ConfirmationService } from 'primeng/api';
+import { ButtonProps } from 'primeng/button';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ConfirmDialogService {
+  private confirmationService: ConfirmationService = inject(ConfirmationService);
+
   confirm(config: ConfirmDialogConfig): Promise<boolean> {
-    return new Promise(resolve => {
-      const confirmed = window.confirm(config.message);
-      resolve(confirmed);
+    const rejectButtonProps = {
+      ...config,
+      label: config.rejectButtonProps?.label ?? 'Não',
+      severity: config.rejectButtonProps?.severity ?? 'secondary'
+    }
+    const acceptButtonProps = {
+      ...config,
+      label: config.rejectButtonProps?.label ?? 'Sim',
+      severity: config.rejectButtonProps?.severity ?? 'danger'
+    }
+
+    return new Promise((resolve) => {
+      this.confirmationService.confirm({
+        header: config.header ?? 'Atenção',
+        message: config.message,
+        icon: config.icon,
+        rejectButtonProps: rejectButtonProps,
+        acceptButtonProps: acceptButtonProps,
+        accept: () => resolve(true),
+        reject: () => resolve(false)
+      })
     });
   }
 }
