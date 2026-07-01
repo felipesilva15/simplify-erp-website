7284b2d26c99a6a7b8bf53136ae3d5e00de876ac
felipe.silva <felipe.silva@pwi.com.br>
2026-07-01 20:11:02 -0300

    test: Criando testes para os Pipes

--- src/app/shared/pipes/phone-pipe.spec.ts
+++ src/app/shared/pipes/phone-pipe.spec.ts
@@ -1,8 +1,33 @@
 import { PhonePipe } from './phone-pipe';
 
 describe('PhonePipe', () => {
-  it('create an instance', () => {
-    const pipe = new PhonePipe();
+  let pipe: PhonePipe;
+
+  beforeEach(() => {
+    pipe = new PhonePipe();
+  });
+
+  it('should create an instance of pipe', () => {
     expect(pipe).toBeTruthy();
+  });
+
+  it('should transform a string of cell phone to formatted number', () => {
+    const formattedValue: string = pipe.transform('11986854256')
+    expect(formattedValue).toBe('(11) 98685-4256');
+  });
+
+  it('should transform a string of telephone to formatted number', () => {
+    const formattedValue: string = pipe.transform('1159825684')
+    expect(formattedValue).toBe('(11) 5982-5684');
+  });
+
+  it('should return empty string for a empty entry', () => {
+    const formattedValue: string = pipe.transform('')
+    expect(formattedValue).toBe('');
+  });
+
+  it('should return the same entry for a invalid phone number', () => {
+    const formattedValue: string = pipe.transform('59852266')
+    expect(formattedValue).toBe('59852266');
   });
 });
