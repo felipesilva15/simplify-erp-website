3e3948c4df1d2620f007c8880c5933e6fb19236f
felipe.silva <felipe.silva@pwi.com.br>
2026-08-01 21:00:49 -0300

    feat: Criando págona de acesso negado

--- src/app/features/error/pages/forbidden/forbidden.component.ts
+++ src/app/features/error/pages/forbidden/forbidden.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { DividerModule } from 'primeng/divider';
+import { MessageModule } from 'primeng/message';
+import { ButtonDirective, ButtonModule } from "primeng/button";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-forbidden',
-  imports: [],
+  imports: [
+    DividerModule,
+    MessageModule,
+    ButtonModule,
+    RouterLink
+],
   templateUrl: './forbidden.component.html',
   styleUrl: './forbidden.component.scss',
 })
