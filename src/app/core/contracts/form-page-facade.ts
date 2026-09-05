190a571ec63be74ea6301c9ae6804732704b3238
felipesilva15 <felipe.allware@gmail.com>
2026-09-05 00:20:10 -0300

    refactor: Ajuste no contrato do FormPageFacade para desconsiderar o entityResponse e usar propriedades diretas

--- src/app/core/contracts/form-page-facade.ts
+++ src/app/core/contracts/form-page-facade.ts
@@ -1,10 +1,11 @@
 import { KeyValue } from '@angular/common';
 import { Signal } from '@angular/core';
 import { BaseEntity } from '../models/base-entity';
-import { ApiResponse } from '../models/api-response';
+import { ApiMetaType } from '../types/api-meta-type';
 export interface FormPageFacade<T extends BaseEntity> {
     entity: Signal<T | null>;
-    entityResponse: Signal<ApiResponse<T> | null>
+    meta: Signal<ApiMetaType | null>;
+    warnings: Signal<string[]>;
     hasWarnings: Signal<boolean>;
     hasServerErrors: Signal<boolean>;
     serverErrors: Signal<KeyValue<string, string>[]>;
